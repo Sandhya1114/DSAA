a7b4749cf0b6a1d3d07fb3011761697541d13bc3
Sandhya <sandhyarana07610761@gmail.com>
2025-07-11 12:32:58 +0530

    commit

--- lec_3IfElse.cpp
+++ lec_3IfElse.cpp
@@ -1,34 +1,34 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int a;
-//     cout<<"enter a num" <<endl;
-//     cin>>a;
-//     // cout<<"number is a "<<a<<endl;
-//     if(a>0){
-//       cout<<"a is postive";
-//     }
-//     else{
-//         cout<<"a is negative";
-//     }
-// }
 #include<iostream>
 using namespace std;
-
 int main(){
-    int a;
-    cout << "Enter a number: " << endl;
-    cin >> a;
-
-    // Optional: Show the number entered
-    // cout << "Number is " << a << endl;
-
-    if(a > 0){
-        cout << "a is positive";
+    // int a;
+    // cout<<"enter a num" <<endl;
+    // cin>>a;
+    // // cout<<"number is a "<<a<<endl;
+    // if(a>0){
+    //   cout<<"a is postive";
+    // }
+    // else{
+    //     cout<<"a is negative";
+    // }
+    int a,b;
+    cout<<"enter A:";
+    cin>>a;
+    cout<<endl<<"enter B:";
+    cin>>b;
+    if(a>b){
+        cout<<'A';
+    }else{
+        cout<<'B';
     }
-    else{
-        cout << "a is negative";
+    // cin.get();
+    //can be take space tap 
+    if(a=0){
+        cout<<"a is zero";
+    }else if(a>0){
+        cout<<"a is greater";
+    }else{
+        cout<<"a is greater";
     }
 
-    return 0;
 }
