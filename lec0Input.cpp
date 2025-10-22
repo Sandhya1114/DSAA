816ded5e0e1711364e71c9f300cddf4280598ea9
Sandhya <sandhyarana07610761@gmail.com>
2025-10-23 00:10:53 +0530

    commit

--- lec0Input.cpp
+++ lec0Input.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 using namespace std;
+void printName(string noni){
+    cout<<"hey "<<noni;
+}
 int main(){
     // int x,y;
     // cin>>x>>y;
@@ -8,15 +11,57 @@ int main(){
     // string str;
     // getline(cin,str);
     // cout<<str;
-    int age;
-    cin>>age;
-    if((age<18) && (age>0)){
-        cout<<"not allowed";
-    }
-    else if(age>=18){
-        cout<<"allowed";
-    }else{
-        cout<<"invalid age";
-    };
+    // int age;
+    // cin>>age;
+    // if((age<18) && (age>0)){
+    //     cout<<"not allowed";
+    // }
+    // else if(age>=18){
+    //     cout<<"allowed";
+    // }else{
+    //     cout<<"invalid age";
+    // };
+    // int day;
+    // cin>>day;
+    // switch(day){
+    //     case 1:
+    //     cout<<"wear white dress";
+    //     break;
+    //     case 2:
+    //     cout<<"wear blue dress";
+    //     break;
+    //     case 3:
+    //     cout<<"wear green dress";
+    //     break;
+    //     case 4:
+    //     cout<<"wear red dress";
+    //     break;
+    //     case 5:
+        
+    //     cout<<"wear yellow dress";
+    //     break;
+    //     case 6:
+    //     cout<<"wear pink dress";
+    //     break;
+    //     case 7:
+    //     cout<<"wear casual";
+    //     break;
+    //     default:
+    //     cout<<"invalid";
+    //     break;
+    // }
+    // int arr[6];
+    // cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4]>>arr[5];
+    // arr[5] *=10;
+    // cout<<arr[5];
+    // string str="sadhya";
+    // int len=str.size();
+    // str[len-1]='A';
+    // cout<<str[len-1];
+    // cout<<str;
+    string name;
+    cin>>name;
+    printName(name);
+
      return 0;
 }
