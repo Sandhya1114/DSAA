c2717bd0016204b01d611697f57d7b1b2e013f28
Sandhya <sandhyarana07610761@gmail.com>
2025-10-22 22:56:53 +0530

    commit

--- lec0Input.cpp
+++ lec0Input.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x;
-    cin>>x;
-    cout<<x;
-    return 0;
+    // int x,y;
+    // cin>>x>>y;
+    // cout<<"value of x is :"<<x<<"value of y is "<<y;
+   
+    // string str;
+    // getline(cin,str);
+    // cout<<str;
+    int age;
+    cin>>age;
+    if((age<18) && (age>0)){
+        cout<<"not allowed";
+    }
+    else if(age>=18){
+        cout<<"allowed";
+    }else{
+        cout<<"invalid age";
+    };
+     return 0;
 }
